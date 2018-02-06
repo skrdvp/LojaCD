4e7ec9d92903b5a2920f17d502158f41fb551f9f
skrdvp <markson@protonmail.com>
2018-02-06 19:36:30 -0300

    Correção Banco

--- src/com/jsf/util/JPAUtil.java
+++ src/com/jsf/util/JPAUtil.java
@@ -6,7 +6,7 @@ import javax.persistence.Persistence;
 
 public class JPAUtil {
 
-	private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("JSF");
+	private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("LojaCD");
 	
 	public static EntityManager getEntityManager(){
 		return emf.createEntityManager();
